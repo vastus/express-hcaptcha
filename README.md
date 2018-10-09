7db5ba2478641e5bc11b404af55be3732ec06de3
Juho Hautala <juho.hautala@helsinki.fi>
2018-10-09 23:45:06 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# express-hcaptcha
+
+Validate your hCaptcha token using a middleware.
+
+## Usage
+
+```
+npm install --save express-hcaptcha
+```
+
+```js
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const hcaptcha = require('express-hcaptcha');
+
+// your hcaptcha secret key
+const SECRET = process.env.HCAPTCHA_SECRET_KEY;
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(bodyParser.json()); // required by express-hcaptcha
+
+// validate the token and proceed to the route when token is valid
+// the middleware also sets the req.hcaptcha to what ever the verify call returns
+app.post('/verify', hcaptcha.middleware.validate(SECRET), (req, res) => {
+  res.json({message: 'verified!', hcaptcha: req.hcaptcha});
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on http://0.0.0.0:${PORT}`);
+});
+```
---
+++ index.js
@@ -0,0 +1,36 @@
+const hcaptcha = require('hcaptcha');
+
+// validate takes an hCaptcha secret and returns
+// an express middleware function
+const validate = (secret) => (req, res, next) => {
+  // get token from the body
+  // requires the body parser JSON middleware
+  // on the app that uses this middleware
+  const token = req.body.token;
+
+  // call next with an error if no token present
+  if (!token) {
+    const err = new Error('bad request - no token provided in body');
+    err.status = 400;
+    return next(err);
+  }
+
+  // verify the hcaptcha and continue on success
+  // call next with an error if verification errors or fails
+  hcaptcha.verify(secret, token)
+    .then((data) => {
+      console.log('data', data);
+      req.hcaptcha = data;
+      if (data.success) {
+        return next();
+      }
+      const err = new Error(`bad request - ${data['error-codes']}`);
+      err.status = 400;
+      return next(err);
+    })
+    .catch(next);
+};
+
+module.exports.middleware = {
+  validate,
+};
